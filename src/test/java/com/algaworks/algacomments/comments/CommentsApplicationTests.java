47caa8db9c8c2d65f1b3baa4f6f09691cbe4335f
jucami90 <jucami90@gmail.com>
2025-11-23 23:13:02 -0500

    add project

---
+++ src/test/java/com/algaworks/algacomments/comments/CommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algacomments.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
